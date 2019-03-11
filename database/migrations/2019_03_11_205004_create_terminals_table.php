e745797acb4a677fcf8501aa46ae50b8fcb73c0a
crcesar15 <crcesar15@gmail.com>
2019-03-11 17:33:53 -0400

    Creados los modelos, migraciones, factorias y seeders de la tabla terminal

---
+++ database/migrations/2019_03_11_205004_create_terminals_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTerminalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('terminals', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->double('lat',8,6);
+            $table->double('lng',8,6);
+            $table->integer('connections');
+            $table->integer('busy');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('terminals');
+    }
+}
--- database/migrations/2019_03_11_210725_create_tabs_table.php
+++ database/migrations/2019_03_11_210725_create_tabs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateTabsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tabs', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->double('lat',8,6);
+            $table->double('lng',8,6);
+            $table->integer('connections');
+            $table->integer('busy');
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tabs');
     }
 }
